c2da1761ae802051781da14e2b00b5eb4c1526a2
Sanchit Walia <swalia_be18@thapar.edu>
2020-12-19 00:01:49 +0530

    1)added Constants class to hold our Exercise Model 2)updated UI to hold photo of exercise 3)fully integrated rest time and exercise time with automatic change to next Exercise. 4)added text to speech functionality

--- app/src/main/java/com/example/quickworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/quickworkout/ExerciseActivity.kt
@@ -3,6 +3,7 @@ package com.example.quickworkout
 import android.graphics.Color
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -12,14 +13,20 @@ import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.quickworkout.databinding.ActivityExerciseBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener{
 
     var mCountDownTimer: CountDownTimer? = null
     var mTimerDuration : Long = 10000
     var mRemainingTime: Long = mTimerDuration
     var mTimeElapsed : Long = 0
     private lateinit var binding: ActivityExerciseBinding
+    private var mExerciseList : ArrayList<ExerciseModel>? = null
+    private var presentExercisePosition = -1
+
+    private var tts : TextToSpeech? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +35,12 @@ class ExerciseActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        tts = TextToSpeech(this, this)
+        mExerciseList = Constants.defaultExerciseList()
         setUpToolBar()
         setupRestView()
+
+
     }
 
     private fun setUpToolBar(){
@@ -49,13 +60,20 @@ class ExerciseActivity : AppCompatActivity() {
 
             }
             override fun onFinish() {
-               // Toast.makeText(this@ExerciseActivity, "Timer is Finished", Toast.LENGTH_SHORT).show()
+                presentExercisePosition++
                 setupExerciseView()
             }
         }.start()
     }
 
     private fun setupRestView(){
+        binding.llExerciseView.visibility = View.GONE
+        binding.llRestView.visibility  = View.VISIBLE
+        if(presentExercisePosition < mExerciseList!!.size - 1) {
+            binding.tvRestViewExerciseName.text =
+                mExerciseList!![presentExercisePosition + 1].getName()
+            speakOut("Be ready for " + mExerciseList!![presentExercisePosition + 1].getName())
+        }
         mCountDownTimer?.cancel()
         mTimeElapsed = 0
         mTimerDuration = 10000
@@ -63,6 +81,9 @@ class ExerciseActivity : AppCompatActivity() {
     }
 
     private fun setupExerciseView(){
+        speakOut( mExerciseList!![presentExercisePosition].getName())
+        binding.ivExercise.setImageResource(mExerciseList!![presentExercisePosition].getImage())
+        binding.tvExerciseName.text = mExerciseList!![presentExercisePosition].getName()
         mCountDownTimer?.cancel()
         mTimeElapsed = 0
         mTimerDuration = 30000
@@ -82,8 +103,36 @@ class ExerciseActivity : AppCompatActivity() {
 
             }
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity, "Timer is Finished", Toast.LENGTH_SHORT).show()
+                if(presentExercisePosition < mExerciseList!!.size - 1)
+                    setupRestView()
             }
         }.start()
+    }
+
+    override fun onInit(status: Int) {
+        if(status == TextToSpeech.SUCCESS){
+            val result = tts!!.setLanguage(Locale.ENGLISH)
+            if(result == TextToSpeech.LANG_MISSING_DATA || result ==
+                    TextToSpeech.LANG_NOT_SUPPORTED)
+                        Log.e("TTS", "the language provided is not supported")
+            else{
+                Log.e("TTS", "Initialization failed")
+            }
+        }
+    }
+
+    private fun speakOut(text : CharSequence){
+        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    override fun onDestroy() {
+
+        if(mCountDownTimer != null) mCountDownTimer!!.cancel()
+        if(tts!= null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+
+        super.onDestroy()
     }
 }
